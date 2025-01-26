00ec14738c9b989e2e55fa4f6dd1f69134922eb8
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-01-26 15:39:34 +0200

    have a probple with modal image

--- src/js/password-code.js
+++ src/js/password-code.js
@@ -10,15 +10,7 @@ refs.verifyButton.addEventListener('click', e => {
 
   const markup = `
   <div class="modal-window">
-    <picture>
-        <source srcset="/img/create-page/modal-check-img.webp 1x, /img/create-page/modal-check-img@2x.webp 2x"
-            media="(min-width: 1200px)" type="image/webp" />
-        <source srcset="/img/create-page/modal-check-img.png 1x, /img/create-page/modal-check-img@2x.png 2x"
-            media="(min-width: 1200px)" type="image/png" />
-        <img class="modal-check-image" src="/img/create-page/modal-check-img.png" alt="A check image" />
-    </picture>
-
-
+    <img class="modal-check-image" src="./img/create-page/modal-check-img.png" alt="A check image" />
     <h2 class="modal-title">Password Changed Successfully</h2>
     <p class="modal-text">Your password has been updated successfully</p>
     <a class="modal-link button" href="./login-account.html">Back to Login</a>
