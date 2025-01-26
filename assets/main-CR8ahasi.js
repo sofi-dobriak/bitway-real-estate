d942a33b97259d88265d17d009a9b2f7244f0c2b
sofi-dobriak <167961175+sofi-dobriak@users.noreply.github.com>
2025-01-26 11:28:19 +0000

    Deploying to gh-pages from @ sofi-dobriak/bitway-real-estate@63533541e86e0c6ed1bcd8328dbe0fe1725fc74d 🚀

---
+++ assets/main-CR8ahasi.js
@@ -0,0 +1,15 @@
+import{b as a}from"./vendor-CAXQE2hQ.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&s(c)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();const n={verifyButton:document.querySelector(".js-verify-button")};n.verifyButton.addEventListener("click",o=>{o.preventDefault(),a.create(`
+  <div class="modal-window">
+    <picture>
+        <source srcset="/img/create-page/modal-check-img.webp 1x, /img/create-page/modal-check-img@2x.webp 2x"
+            media="(min-width: 1200px)" type="image/webp" />
+        <source srcset="/img/create-page/modal-check-img.png 1x, /img/create-page/modal-check-img@2x.png 2x"
+            media="(min-width: 1200px)" type="image/png" />
+        <img class="modal-check-image" src="/img/create-page/modal-check-img.png" alt="A check image" />
+    </picture>
+
+    <h2 class="modal-title">Password Changed Successfully</h2>
+    <p class="modal-text">Your password has been updated successfully</p>
+    <a class="modal-link button" href="./login-account.html">Back to Login</a>
+</div>`).show()});
+//# sourceMappingURL=main-CR8ahasi.js.map
