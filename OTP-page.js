959cdf6c933f229c61056505a629b65e50fd7d92
sofi-dobriak <167961175+sofi-dobriak@users.noreply.github.com>
2025-01-26 13:21:59 +0000

    Deploying to gh-pages from @ sofi-dobriak/bitway-real-estate@2a005dfe9405eca50f7f49a38e28790a6f8f88e7 🚀

--- OTP-page.js
+++ OTP-page.js
@@ -1,11 +1,11 @@
 import"./assets/styles-B6CXeTBy.js";import{b as c}from"./assets/vendor-CAXQE2hQ.js";const e={verifyButton:document.querySelector(".js-verify-button")};console.log(e.verifyButton);e.verifyButton.addEventListener("click",a=>{a.preventDefault(),c.create(`
   <div class="modal-window">
     <picture>
-        <source srcset="./img/create-page/modal-check-img.webp 1x, ./img/create-page/modal-check-img@2x.webp 2x"
+        <source srcset="../../img/create-page/modal-check-img.webp 1x, ../../img/create-page/modal-check-img@2x.webp 2x"
             media="(min-width: 1200px)" type="image/webp" />
-        <source srcset="./img/create-page/modal-check-img.png 1x, ./img/create-page/modal-check-img@2x.png 2x"
+        <source srcset="../../img/create-page/modal-check-img.png 1x, ../../img/create-page/modal-check-img@2x.png 2x"
             media="(min-width: 1200px)" type="image/png" />
-        <img class="modal-check-image" src="./img/create-page/modal-check-img.png" alt="A check image" />
+        <img class="modal-check-image" src="../../img/create-page/modal-check-img.png" alt="A check image" />
     </picture>
 
 
