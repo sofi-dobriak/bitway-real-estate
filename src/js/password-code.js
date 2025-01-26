014424a41aff53176b7481939e8105fc3d533ddc
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-01-26 15:48:07 +0200

    123

--- src/js/password-code.js
+++ src/js/password-code.js
@@ -3,19 +3,24 @@ import * as basicLightbox from 'basiclightbox';
 const refs = {
   verifyButton: document.querySelector('.js-verify-button'),
 };
-console.log(refs.verifyButton);
 
 refs.verifyButton.addEventListener('click', e => {
   e.preventDefault();
 
   const markup = `
   <div class="modal-window">
-    <img class="modal-check-image" src="./img/create-page/modal-check-img.png" alt="A check image" />
-    <h2 class="modal-title">Password Changed Successfully</h2>
+<picture>
+      <source srcset="./img/create-page/modal-check-img.webp 1x, ./img/create-page/modal-check-img@2x.webp 2x"
+          media="(min-width: 1200px)" type="image/webp" />
+      <source srcset="./img/create-page/modal-check-img.png 1x, ./img/create-page/modal-check-img@2x.png 2x"
+          media="(min-width: 1200px)" type="image/png" />
+      <img class="modal-check-image" src="./img/create-page/modal-check-img.png" alt="A check image" />
+  </picture>
+  <h2 class="modal-title">Password Changed Successfully</h2>
     <p class="modal-text">Your password has been updated successfully</p>
     <a class="modal-link button" href="./login-account.html">Back to Login</a>
 </div>`;
-  const instance = basicLightbox.create(markup);
 
+  const instance = basicLightbox.create(markup);
   instance.show();
 });
