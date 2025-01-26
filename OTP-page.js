7245085d9a2628a0e140be7e201de7601ee9d83b
sofi-dobriak <167961175+sofi-dobriak@users.noreply.github.com>
2025-01-26 13:39:51 +0000

    Deploying to gh-pages from @ sofi-dobriak/bitway-real-estate@00ec14738c9b989e2e55fa4f6dd1f69134922eb8 🚀

--- OTP-page.js
+++ OTP-page.js
@@ -1,14 +1,6 @@
-import"./assets/styles-B6CXeTBy.js";import{b as c}from"./assets/vendor-CAXQE2hQ.js";const e={verifyButton:document.querySelector(".js-verify-button")};console.log(e.verifyButton);e.verifyButton.addEventListener("click",a=>{a.preventDefault(),c.create(`
+import"./assets/styles-B6CXeTBy.js";import{b as a}from"./assets/vendor-CAXQE2hQ.js";const e={verifyButton:document.querySelector(".js-verify-button")};console.log(e.verifyButton);e.verifyButton.addEventListener("click",t=>{t.preventDefault(),a.create(`
   <div class="modal-window">
-    <picture>
-        <source srcset="/img/create-page/modal-check-img.webp 1x, /img/create-page/modal-check-img@2x.webp 2x"
-            media="(min-width: 1200px)" type="image/webp" />
-        <source srcset="/img/create-page/modal-check-img.png 1x, /img/create-page/modal-check-img@2x.png 2x"
-            media="(min-width: 1200px)" type="image/png" />
-        <img class="modal-check-image" src="/img/create-page/modal-check-img.png" alt="A check image" />
-    </picture>
-
-
+    <img class="modal-check-image" src="./img/create-page/modal-check-img.png" alt="A check image" />
     <h2 class="modal-title">Password Changed Successfully</h2>
     <p class="modal-text">Your password has been updated successfully</p>
     <a class="modal-link button" href="./login-account.html">Back to Login</a>
